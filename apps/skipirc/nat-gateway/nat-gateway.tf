c18bd765655205a77a996254815bf3faa1499c66
Daniel Tharp <github@nags.me>
2022-05-19 10:51:07 -0600

    Add aws_eip resources

--- apps/skipirc/nat-gateway/nat-gateway.tf
+++ apps/skipirc/nat-gateway/nat-gateway.tf
@@ -1,5 +1,14 @@
+resource "aws_eip" "virginia" {
+  vpc = true
+}
+
 resource "aws_nat_gateway" "virginia" {
   subnet_id = "subnet-0fe48a641e328bb56"
+}
+
+resource "aws_eip" "california" {
+  vpc = true
+  provider = aws.us_west_1
 }
 
 resource "aws_nat_gateway" "california" {
