78ec1c7d2432126f657d2a3198d50e73eae0e282
Daniel Tharp <github@nags.me>
2022-05-19 13:57:36 -0600

    Use correct subnets, add routes

--- apps/skipirc/nat-gateway/nat-gateway.tf
+++ apps/skipirc/nat-gateway/nat-gateway.tf
@@ -1,19 +1,16 @@
-resource "aws_eip" "blackjack" {
+resource "aws_eip" "virginia" {
   vpc = true
 }
 
-resource "aws_nat_gateway" "blackjack" {
-  subnet_id = "subnet-0fe48a641e328bb56"
-  allocation_id = aws_eip.blackjack.allocation_id
+resource "aws_nat_gateway" "virginia" {
+  subnet_id = "subnet-0f8d498cd98a23993"
+  allocation_id = aws_eip.virginia.allocation_id
 }
 
-resource "aws_eip" "services" {
-  vpc = true
-}
-
-resource "aws_nat_gateway" "services" {
-  subnet_id = "subnet-02966c74743b1df28"
-  allocation_id = aws_eip.services.allocation_id
+resource "aws_route" "virginia" {
+  route_table_id            = "rtb-0d9800d9819e5fed5"
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.virginia.id
 }
 
 resource "aws_eip" "california" {
@@ -22,7 +19,14 @@ resource "aws_eip" "california" {
 }
 
 resource "aws_nat_gateway" "california" {
-  subnet_id = "subnet-07af25260e27a3dfd"
+  subnet_id = "subnet-00da7d682634da8e8"
   allocation_id = aws_eip.california.allocation_id
+  provider = aws.us_west_1
+}
+
+resource "aws_route" "california" {
+  route_table_id            = "rtb-08f6c068ec2d2868b"
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.california.id
   provider = aws.us_west_1
 }
